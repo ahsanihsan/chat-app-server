290c97a695d0bb61ccb09afe1d9ece9f04bf847d
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2019-12-04 11:54:27 +0500

    Friends complete info added

--- src/handlers/loadFriends.js
+++ src/handlers/loadFriends.js
@@ -4,12 +4,16 @@ const User = require("../models/User");
 module.exports = async function(request, reply) {
   let promise = new Promise(async resolve => {
     await User.findOne({ email: request.auth.credentials.email })
-      .populate("friends", "fullName")
+      .populate("friends")
       .then(user => {
         if (user) {
           const mappedFriends = {};
           user.friends.forEach(friend => {
-            mappedFriends[friend._id] = friend;
+            mappedFriends[friend._id] = {
+              _id: friend._id,
+              email: friend.email,
+              name: friend.fullName
+            };
           });
           resolve(mappedFriends);
         } else {
