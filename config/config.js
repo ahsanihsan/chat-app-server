a0f673548a4472fce63aa92569f10d94d3b4e6f5
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2019-12-16 16:01:53 +0100

    Ok

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   database: "mongodb://localhost:27017/message_app",
   server: {
     port: 8888,
-    host: "localhost"
+    host: "0.0.0.0"
   },
   jwt: {
     secret: "ahsan11343",
